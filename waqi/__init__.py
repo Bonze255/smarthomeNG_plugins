0c528b4ea4f6b447aba9443bcf3ffda82e67d6cd
Bonze255 <fragger255@hotmail.com>
2020-02-25 21:14:27 +0100

    Update __init__.py

--- waqi/__init__.py
+++ waqi/__init__.py
@@ -1,100 +1,256 @@
-import os
-import json 
+#
+#!/usr/bin/env python3
+# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#
+####################################################################################
+######################################################################################
+#
+#  Copyright 2020 Version-1    Manuel Holländer
 
+####################################################################################
+####################################################################################
+#
+#   VERSION - 1
+#
+####################################################################################
+# 
+import logging
 import requests
-class WAQI():
-    #http://flothesof.github.io/world-air-quality-pollution-maps.html
-    def __init__(self):
-        self._data = []
+import json 
+import threading
+
+from lib.model.smartplugin import *
+from lib.module import Modules
+from lib.item import Items
+
+class Waqi(SmartPlugin):
+    ALLOW_MULTIINSTANCE = False
+    PLUGIN_VERSION="1.0.0"
+    
+    def __init__(self, smarthome,city='/', token='',cycle=20):
+        self._city= str(city)
+        self._cycle = int(cycle)
+        self._token = str(token)
+        self._sh = smarthome
+        self.logger = logging.getLogger(__name__)
+        self.messages = {}
+        self._lock = threading.Lock()
+
         self.base_url = "https://api.waqi.info"
-        self.token = "ae2c2b905e7bddc91c967a5a4ea9d6cdc757f9cb"
-        self.city = 'germany/rheinlandpfalz/kaiserslautern-rathausplatz'
-        self.getData()
+        #self.token = "ae2c2b905e7bddc91c967a5a4ea9d6cdc757f9cb"
+        #self.city = 'germany/rheinlandpfalz/kaiserslautern-rathausplatz'
+        self._read()
         
-    def getData(self):
-        r = requests.get(self.base_url + f"/feed/"+self.city+"/?token="+self.token)
-        if r.status_code == 200:
-            print(r.json())
-            aqi_data = r.json()['data']['iaqi']
-            self._data['waqi_no2'] = r.json()['data']['iaqi']['no2']['v']
-            self._data['waqi_o3'] = r.json()['data']['iaqi']['o3']['v']
-            self._data['waqi_p'] = r.json()['data']['iaqi']['p']['v']
-            self._data['waqi_pm10'] = r.json()['data']['iaqi']['pm10']['v']
-            self._data['waqi_t'] = r.json()['data']['iaqi']['t']['v']
+        self._sh.scheduler.add('Waqi read cycle', self._read, prio=5, cycle=self._cycle)
+        if not self.init_webinterface():
+            self._init_complete = False
             
-            print(r.json()['data']['iaqi'])
-            print("no2 ",r.json()['data']['iaqi']['no2']['v'])
-            print("o3 ",r.json()['data']['iaqi']['o3']['v'])
-            print("p ",r.json()['data']['iaqi']['p']['v'])
-            print("pm10 ",r.json()['data']['iaqi']['pm10']['v'])
-            print("t ",r.json()['data']['iaqi']['t']['v'])
-            print("time ",r.json()['data']['time']['v'])
-            #{'no2': {'v': 9.6}, 'o3': {'v': 17.9}, 'p': {'v': 1028.9}, 'pm10': {'v': 3}, 't': {'v': 1.6}}
-            #print("City: {}, Air quality index: {}".format(r.json()['data']['city']['name'], r.json()['data']['aqi']))
-        else:
-            print("Fehler bei der Datenabfrage")
+    # ----------------------------------------------------------------------------------------------
+    # Daten Lesen, über SHNG bei item_Change
+    # ----------------------------------------------------------------------------------------------
+    def groupread(self, ga, dpt):
+        pass
+        
+    # ----------------------------------------------------------------------------------------------
+    # Daten Lesen, zyklisch
+    # ----------------------------------------------------------------------------------------------     
 
-WAQI()
+    
+    def _read(self):
+        self._data = {}
+        try:
+            r = requests.get(self.base_url + f"/feed/"+self._city+"/?token="+self._token)
+            if r.status_code == 200:
+                for value in r.json()['data']['iaqi']:
+                    self.logger.debug("Waqi: request {}".format(value))
+                    self._data[value] = r.json['data']['iaqi'][value]['v']
+                    #self._data['data'].update(self._data[value])
+                #self._data['no2'] = r.json()['data']['iaqi']['no2']['v']
+                #self._data['o3'] = r.json()['data']['iaqi']['o3']['v']
+                #self._data['p'] = r.json()['data']['iaqi']['p']['v']
+                #self._data['pm10'] = r.json()['data']['iaqi']['pm10']['v']
+                #self._data['t'] = r.json()['data']['iaqi']['t']['v']
+                #self._data['w'] = r.json()['data']['w']
+                #self._data['wg'] = r.json()['data']['wg']
+                #self._data['aqi'] = r.json()['data']['aqi']
+                #self._data['city'] = r.json()['data']['city']['name']
+                self.logger.debug("Waqi: request {}".format(r.json()))
+                #self._data['data'].update({city:r.json['data']['city'][name]})
+                #self._data['data'] = {'no2':self._data['no2'],'o3':self._data['o3'],'p':self._data['p'],'pm10':self._data['pm10'],'t':self._data['t'],'aqi':self._data['aqi']}
+            else:
+                 self.logger.error("Waqi: Reading ERROR from Waqi")
+        except Exception as e:
+                self.logger.error("Waqi: Error {}".format(e))
+
+        for x in self._data:
+            if x in self.messages:
+                self.logger.debug("Waqi: Update item {1} mit key {0} = {2}".format(x, self.messages[x], self._data[x]))
+                item = self.messages[x]
+                item(self._data[x], 'Waqi')
+                
+
+    # ----------------------------------------------------------------------------------------------
+    # Befehl senden, wird aufgerufen wenn sich item  mit robvac ändert!
+    # ----------------------------------------------------------------------------------------------
+    def update_item(self, item, caller=None, source=None, dest=None):
+        if caller != 'Waqi':
+             pass
+    def run(self):
+        self.alive = True
+
+    def stop(self):
+        self.alive = False
+        
+    def parse_item(self, item):
+        if self.has_iattr(item.conf, 'waqi'):
+            message = self.get_iattr_value(item.conf, 'waqi')
+            self.logger.debug("Waqi: {0} keyword {1}".format(item, message))
+
+            if not message in self.messages:
+                self.messages[message] = item
+                
+            return self.update_item
+    
+    def update_item_read(self, item, caller=None, source=None, dest=None):
+        if self.has_iattr(item.conf, 'waqi'):
+            for message in item.get_iattr_value(item.conf, 'waqi'):
+
+                self.logger.debug("Waqi: update_item_read {0}".format(message))
+            
+# ------------------------------------------
+#    Webinterface Methoden
+# ------------------------------------------   
+    def init_webinterface(self):
+        """"
+        Initialize the web interface for this plugin
+
+        This method is only needed if the plugin is implementing a web interface
+        """
+        try:
+            self.mod_http = Modules.get_instance().get_module(
+                'http')  # try/except to handle running in a core version that does not support modules
+        except:
+            self.mod_http = None
+        if self.mod_http == None:
+            self.logger.error("Plugin '{}': Not initializing the web interface".format(self.get_shortname()))
+            return False
+
+        # set application configuration for cherrypy
+        webif_dir = self.path_join(self.get_plugin_dir(), 'webif')
+        config = {
+            '/': {
+                'tools.staticdir.root': webif_dir,
+            },
+            '/static': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': 'static'
+            }
+        }
+        
+        self.logger.debug("Plugin Debug ausgabe '{0}': {1}, {2}, {3}, {4}, {5}".format(self.get_shortname(), webif_dir, self.get_shortname(),config,  self.get_classname(), self.get_instance_name()))
+        # Register the web interface as a cherrypy app
+        self.mod_http.register_webif(WebInterface(webif_dir, self),
+                                     self.get_shortname(),
+                                     config,
+                                     self.get_classname(), self.get_instance_name(),
+                                     description='')
+
+        return True
+
+# ------------------------------------------
+#    Webinterface Methoden
+# ------------------------------------------   
+
+    def get_connection_info(self):
+        info = {}
+        info['city'] = self._city
+        info['token'] = self._token
+        info['cycle'] = self._cycle
+        return info
+        
+    def init_webinterface(self):
+        """"
+        Initialize the web interface for this plugin
+
+        This method is only needed if the plugin is implementing a web interface
+        """
+        try:
+            self.mod_http = Modules.get_instance().get_module(
+                'http')  # try/except to handle running in a core version that does not support modules
+        except:
+            self.mod_http = None
+        if self.mod_http == None:
+            self.logger.error("Plugin '{}': Not initializing the web interface".format(self.get_shortname()))
+            return False
+
+        # set application configuration for cherrypy
+        webif_dir = self.path_join(self.get_plugin_dir(), 'webif')
+        config = {
+            '/': {
+                'tools.staticdir.root': webif_dir,
+            },
+            '/static': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': 'static'
+            }
+        }
+        
+        self.logger.debug("Plugin Debug ausgabe '{0}': {1}, {2}, {3}, {4}, {5}".format(self.get_shortname(), webif_dir, self.get_shortname(),config,  self.get_classname(), self.get_instance_name()))
+        # Register the web interface as a cherrypy app
+        self.mod_http.register_webif(WebInterface(webif_dir, self),
+                                     self.get_shortname(),
+                                     config,
+                                     self.get_classname(), self.get_instance_name(),
+                                     description='')
+
+        return True
 
 
+# ------------------------------------------
+#    Webinterface of the plugin
+# ------------------------------------------
 
- # 'aqi':3,
-   # 'idx':6133,
-   # 'attributions':[
-      # {
-         # 'url':'http://www.luft-rlp.de/aktuell/',
-         # 'name':'Luftreinhaltung Rheinland-Pfalz',
-         # 'logo':'Germany-RheinlandPfalz.png'
-        # },
-      # {
-         # 'url':'https://waqi.info/',
-         # 'name':'World Air Quality Index Project'
-      
-        # }
-   
-    # ],
-   # 'city':{
-      # 'geo':[
-         # 49.446,
-         # 7.7673
-      
-# ],
-      # 'name':'Kaiserslautern-Rathausplatz, Germany',
-      # 'url':'https://aqicn.org/city/germany/rheinlandpfalz/kaiserslautern-rathausplatz'
-   
-# },
-   # 'dominentpol':'pm10',
-   # 'iaqi':{
-      # 'no2':{
-         # 'v':9.6
-      
-# },
-      # 'o3':{
-         # 'v':17.9
-      
-# },
-      # 'p':{
-         # 'v':1028.9
-      
-# },
-      # 'pm10':{
-         # 'v':3
-      
-# },
-      # 't':{
-         # 'v':1.6
-      
-# }
-   
-# },
-   # 'time':{
-      # 's':'2020-02-21 08:00:00',
-      # 'tz':'+01:00',
-      # 'v':1582272000
-   
-# },
-   # 'debug':{
-      # 'sync':'2020-02-21T16:13:37+09:00'
-   
-# }
-# }
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+
+class WebInterface(SmartPluginWebIf):
+
+
+    def __init__(self, webif_dir, plugin):
+        """
+        Initialization of instance of class WebInterface
+
+        :param webif_dir: directory where the webinterface of the plugin resides
+        :param plugin: instance of the plugin
+        :type webif_dir: str
+        :type plugin: object
+        """
+        self.logger = logging.getLogger(__name__)
+        self.webif_dir = webif_dir
+        self.plugin = plugin
+        self.tplenv = self.init_template_environment()
+        self.logger.debug("Plugin : Init Webif")
+        self.items = Items.get_instance()
+
+    @cherrypy.expose
+    def index(self, reload=None):
+        """
+        Build index.html for cherrypy
+        Render the template and return the html file to be delivered to the browser
+        :return: contents of the template after beeing rendered
+        """
+        plgitems = []
+        for item in self.items.return_items():
+            if ('waqi' in item.conf):
+                plgitems.append(item)
+        self.logger.debug("Plugin : Render index Webif")
+        tmpl = self.tplenv.get_template('index.html')
+        return tmpl.render(plugin_shortname=self.plugin.get_shortname(), 
+                            plugin_version=self.plugin.get_version(),
+                            plugin_info=self.plugin.get_info(),
+                            p=self.plugin,
+                            connection = self.plugin.get_connection_info(),
+                            webif_dir = self.webif_dir,
+                            image_snapshots = self.plugin.get_files(),
+                            items=sorted(plgitems, key=lambda k: str.lower(k['_path'])))
+                            
