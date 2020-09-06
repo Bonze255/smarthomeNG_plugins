e60e95a671dfa58444afd73762eb4e19994c5dd9
Bonze255 <fragger255@hotmail.com>
2020-09-06 15:46:57 +0200

    Add files via upload

---
+++ xiaomi_vac/widget/robo.js
@@ -0,0 +1,37 @@
+/// ----- robo.saugroboter ----------------------------------------------------
+$.widget("sv.status_robvac", $.sv.widget, {
+
+	initSelector: '[data-widget="status.robvac"]',
+
+	options: {
+	},
+	
+	_create: function() {
+		this._super();
+		this.element.cycle();
+	},
+
+	_update: function(response) {
+		var items = String(this.options.item).explode();
+		//console.log('test_saugroboter_update');
+		//console.log(items);
+		//console.log(response);
+		if (response[2] == 'Charging'){
+			$(".vac_status").prepend(" -+ Charging +-");
+		}else if (response[2] == 'Cleaning'){
+			$(".vac_status").prepend(" -+ Cleaning +-");
+			$(".vac_status").prepend(" Fläche: "+ items[0] +" m² | </br> Zeit: "+ items[1] +"h");
+		}else if (response[2] == 'Error'){
+			$(".vac_status").prepend(" -+ ERROR +-");
+			$(".vac_status").prepend(" Code: "+ items[3]);
+		};
+	},
+	
+	_repeat: function() {
+
+	},
+
+	_events: {
+	}
+
+});
